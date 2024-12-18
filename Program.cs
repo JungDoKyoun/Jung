d3a7405461ed91d5c613aa1250980788866d4ef6
JungDoKyoun <kyous112@naver.com>
2024-12-18 11:37:56 +0900

    [Feat] 기능 추가
    설명

--- Program.cs
+++ Program.cs
@@ -13,6 +13,9 @@ namespace Temp
             Console.WriteLine("이힣");
             Console.WriteLine("이힣");
             Console.WriteLine("이힣");
+            Console.WriteLine("기능개발 중");
+            Console.WriteLine("막 반복문도 적고, 클래스도 나누고");
+            Console.WriteLine("클래스도 쪼개고, 기능 추가하고");
         }
     }
 }
